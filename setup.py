ab6980b0aa9b53d7b7d838a99859bb3d5fa8330c
Armin Ronacher <armin.ronacher@active-4.com>
2018-02-19 13:57:08 +0100

    0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools.dist import Distribution
 
 setup(
     name='milksnake',
-    version='0.1.1',
+    version='0.1.2',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
     packages=['milksnake'],
